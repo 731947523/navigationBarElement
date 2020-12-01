ea14e95e1502c7d23bdb5a0ee28e3636d151dfd2
731947523 <731947523@qq.com>
2020-12-01 12:39:59 +0800

    1

---
+++ src/views/daohang/Base/form/attrs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      provideObj: this.testAttr
+    };
+  },
+  props: {
+    testAttr: {
+      type: Object
+    }
+  },
+  mounted() {
+    console.log(this, "attrs----组件");
+    console.log(this.$attrs.testAttrs, "attrs----组件", this.provideObj);
+    console.log(this.$listeners, "attrs----组件", this.provideObj);
+  }
+};
+</script>
